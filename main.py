5789a20b3c83f77be0e99d69417a59c30a25d8e8
Sofia58 <firsovasofia1@gmail.com>
2020-10-04 12:23:25 +0700

    dummy

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import random
+
+class Field:
+    cells = []
+
+    def __init__(self, width = 100, height = 100, cell_size = 5, speed = 4):
+        self_width = width
+        self_height = height
+        self_cell_size = cell_size
+        self_screen_size = width*height
+        self_cell_width = self_width//self_cell_size
+        self_cell_height = self_height//self_cell_size
+        self_speed
+    def randomize(self):
+        for x in range(width, height, cell_size):
+            cells.append
+
+    def neighbours(self, x, y):
+        for i in range():
 
 
-if __name__ == '__main__':
-    print("qwertty")
+
+
+
